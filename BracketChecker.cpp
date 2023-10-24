4d9cc4b1fdab5a2bf2f4da068758e81a223b4b98
riamuk101 <rmukherji@smu.edu>
2023-10-24 16:56:16 -0500

    Most recent working

--- BracketChecker.cpp
+++ BracketChecker.cpp
@@ -16,7 +16,7 @@ using namespace std;
 void BracketChecker::checkArray() { 
 int errorLine  = 0;
     //file is read in through passed arg
-    ifstream file("/users7/cse/rmukherji/assignment-3-do-my-brackets-match-riamuk101/test_data/DSVector_good.h");
+    ifstream file("/users7/cse/rmukherji/assignment-3-do-my-brackets-match-riamuk101/test_data/DSVector_bad.h");
     //std::ifstream file("/users7/cse/rmukherji/assignment-3-do-my-brackets-match-riamuk101/test_data/fake.txt");
     if (!file.good())
     {
@@ -37,6 +37,7 @@ while (getline(file,line)){
            else if ((potentialBracket=='}')||(potentialBracket==']')||(potentialBracket==')')) {
           if (bracketStack.isEmpty() == true){
               cout << "brackets don't match. Error on line " << errorLine << endl;
+              return;
           }
           char topBracket = bracketStack.peek();
           if ((potentialBracket == '}' && topBracket == '{') ||
@@ -60,6 +61,7 @@ while (getline(file,line)){
 
 if (bracketStack.isEmpty()==false){
 cout << "brackets don't match. Error on line: " << errorLine << endl;
+return;
 }
 else{
     cout << "brackets match " << endl;
@@ -68,9 +70,9 @@ else{
 }//end checkArray
 
 void BracketChecker::checkList() { 
-
+int errorLine  = 0;
     //file is read in through passed arg
-     ifstream file("/users7/cse/rmukherji/assignment-3-do-my-brackets-match-riamuk101/test_data/DSVector_good.h");
+     ifstream file("/users7/cse/rmukherji/assignment-3-do-my-brackets-match-riamuk101/test_data/DSVector_bad.h");
     //std::ifstream file("/users7/cse/rmukherji/assignment-3-do-my-brackets-match-riamuk101/test_data/fake.txt");
     if (!file.good())
      {
@@ -82,6 +84,7 @@ void BracketChecker::checkList() {
     std :: string line; 
 
 while (getline(file,line)){
+    errorLine++;
     
         for (char potentialBracket : line){
             if ((potentialBracket=='{')||(potentialBracket=='[')||(potentialBracket=='(')){
@@ -90,7 +93,8 @@ while (getline(file,line)){
             }//end if 
            else if ((potentialBracket=='}')||(potentialBracket==']')||(potentialBracket==')')) {
           if (bracketStackList.isEmpty() == true){
-              cout << "brackets don't match ";
+              cout << "brackets don't match. Error on line: " << errorLine << endl;
+              return;
           }
           char topBracket = bracketStackList.peek();
           if ((potentialBracket == '}' && topBracket == '{') ||
@@ -101,7 +105,7 @@ while (getline(file,line)){
         }
         else{
 
-            cout << "brackets don't match ";
+           cout << "brackets don't match. Error on line: " << errorLine << endl;
             return;
         }
        
@@ -113,7 +117,8 @@ while (getline(file,line)){
 
 
 if (bracketStackList.isEmpty()==false){
-    cout << "brackets don't match ";
+    cout << "brackets don't match. Error on line: " << errorLine << endl;
+    return;
 }
 else{
     cout << "brackets match ";
